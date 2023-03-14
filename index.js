b835e7056e6fa65f5c7ef92b2650687d50669461
nihil0r <brice.grivet.1995@gmail.com>
2023-03-14 21:55:56 +0100

    V2.0 Service du fichier client.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Importation des modules nécessaires
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // Création de l'application Express
 const app = express();
@@ -11,36 +12,36 @@ const annotations = {};
 
 // Route pour créer une annotation
 app.post('/annotations', (req, res) => {
-  const { annotationURI, resourceURI, content, format } = req.body;
-  if (!annotationURI || !resourceURI || !content || !format) {
-    return res.status(400).send('Tous les champs sont requis.');
-  }
-  annotations[annotationURI] = { resourceURI, content, format };
-  res.status(201).send('Annotation créée avec succès.');
+    const { annotationURI, resourceURI, content, format } = req.body;
+    if (!annotationURI || !resourceURI || !content || !format) {
+        return res.status(400).send('Tous les champs sont requis.');
+    }
+    annotations[annotationURI] = { resourceURI, content, format };
+    res.status(201).send('Annotation créée avec succès.');
 });
 
 // Route pour récupérer une annotation
 app.get('/annotations/:annotationURI', (req, res) => {
-  const annotationURI = req.params.annotationURI;
-  const annotation = annotations[annotationURI];
-  if (!annotation) {
-    return res.status(404).send('Annotation introuvable.');
-  }
-  res.json(annotation);
+    const annotationURI = req.params.annotationURI;
+    const annotation = annotations[annotationURI];
+    if (!annotation) {
+        return res.status(404).send('Annotation introuvable.');
+    }
+    res.json(annotation);
 });
 
 // Route pour récupérer toutes les annotations portant sur une ressource
 app.get('/resources/:resourceURI/annotations', (req, res) => {
-  const resourceURI = req.params.resourceURI;
-  const resourceAnnotations = Object.values(annotations).filter(
-    (annotation) => annotation.resourceURI === resourceURI
-  );
-  res.json(resourceAnnotations);
+    const resourceURI = req.params.resourceURI;
+    const resourceAnnotations = Object.values(annotations).filter(
+        (annotation) => annotation.resourceURI === resourceURI
+    );
+    res.json(resourceAnnotations);
 });
 
 // Route pour récupérer toutes les annotations de notre serveur
 app.get('/annotations', (req, res) => {
-  res.json(Object.values(annotations));
+    res.json(Object.values(annotations));
 });
 
 // Fonction pour importer des annotations d'un serveur tiers
@@ -49,8 +50,13 @@ async function importAnnotationsFromThirdPartyServer(serverURL) {
   // Ajouter les annotations importées à notre objet "annotations"
 }
 
+// Lignes pour servir le fichier client.html à la racine
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client.html'));
+});
+
 // Démarrage du serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Serveur en écoute sur le port ${PORT}`);
+    console.log(`Serveur en écoute sur le port ${PORT}`);
 });
