aa35e59c3b1649074a50b4d6c794b40f8c9fa16f
nihil0r <brice.grivet.1995@gmail.com>
2023-03-15 12:56:22 +0100

    V2.0 Modif index routes pour récupérer les annotations liées à une ressource

--- index.js
+++ index.js
@@ -35,13 +35,21 @@ app.get('/annotations/:annotationURI', (req, res) => {
 });
 
 // Route pour récupérer toutes les annotations portant sur une ressource
-app.get('/resources/:resourceURI/annotations', (req, res) => {
-    const resourceURI = req.params.resourceURI;
-    const resourceAnnotations = Object.values(annotations).filter(
-        (annotation) => annotation.resourceURI === resourceURI
-    );
-    res.json(resourceAnnotations);
+app.get('/annotations', (req, res) => {
+    const resourceURI = req.query.resourceURI;
+    if (resourceURI) {
+        const resourceAnnotations = Object.values(annotations).filter(
+            (annotation) => annotation.resourceURI === resourceURI
+        );
+        res.json(resourceAnnotations);
+    } else {
+        const annotationsArray = Object.entries(annotations).map(([annotationURI, annotation]) => {
+            return { annotationURI, ...annotation };
+        });
+        res.json(annotationsArray);
+    }
 });
+
 
 // Route pour récupérer toutes les annotations de notre serveur
 app.get('/annotations', (req, res) => {
